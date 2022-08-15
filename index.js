a3acb83ee495e869fe29cc3b82ca80c673dd3fc3
ironman109 <sondinh99999@gmail.com>
2022-08-16 01:37:47 +0700

    dinhthaison

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const server = http.createServer(app)
+const {
+     Server 
+} = require('socket.io')
+
+const io = new Server(server)
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', (socket) => {
+  console.log('user connected')
+  socket.on('on-chat', data => {
+    io.emit('user-chat', data)
+  })
+})
+
+server.listen(3000, () => {
+  console.log('listening on port 3000')
+})
